06a63afd834157e86b24b4991aa43df5ae4b403d
Rehan2007ahmad <154256899+Rehan2007ahmad@users.noreply.github.com>
2025-09-23 16:26:34 +0545

    new update

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,6 +6,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
+      Allow: "/sitemap.xml" 
     },
     sitemap: `${siteUrl}/sitemap.xml`,
   };
