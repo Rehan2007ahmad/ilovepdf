6be6006e417f383b105bb24a562c6777fa25c254
Rehan2007ahmad <154256899+Rehan2007ahmad@users.noreply.github.com>
2025-09-24 09:00:05 +0545

    robots.txt updated

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
-      allow: ["/", "/sitemap.xml" ],
+      allow: "/",
     },
     sitemap: `${siteUrl}/sitemap.xml`,
   };
